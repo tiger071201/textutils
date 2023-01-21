665d696ffb2e9a073e7ce98af6b61d1c9f904f46
mohammed <mdtiger071201@gmail.com>
2023-01-21 16:48:01 +0530

    Updates

---
+++ precache-manifest.313214dd973aef83a8d13ff0d07924ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "456347727bce08b58ba7",
+    "url": "/textutils-REACT/static/css/main.46743ad6.chunk.css"
+  },
+  {
+    "revision": "456347727bce08b58ba7",
+    "url": "/textutils-REACT/static/js/main.45634772.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textutils-REACT/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textutils-REACT/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "cb5b87d921597dcb3f41",
+    "url": "/textutils-REACT/static/js/runtime~main.cb5b87d9.js"
+  },
+  {
+    "revision": "0a0394ac4757316a3630838b1c9726bd",
+    "url": "/textutils-REACT/index.html"
+  }
+];
